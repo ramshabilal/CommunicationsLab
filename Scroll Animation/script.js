466a9bf52e402572f05bb2c4a81518e692deb1ef
Ramsha Bilal <95581771+ramshabilal@users.noreply.github.com>
2022-10-09 21:32:38 +0400

    Add files via upload

---
+++ Scroll Animation/script.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+/*$("#blockOne").mousemove(() => {
+    $("#blockOne").text("Scroll Below to Read")
+})
+*/
+
+window.addEventListener('load', function () { AOS.init(); });
